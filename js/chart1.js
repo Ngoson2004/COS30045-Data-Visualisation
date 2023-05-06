cdfe926993fe5a14585a5c0bcb376ddf384a9d55
Tran Duc Anh <tranducanh@sace.edu.vn>
2023-05-07 08:04:56 +1000

    commit 3rd ( modified layout in index.html, css file )

--- js/chart1.js
+++ js/chart1.js
@@ -1,35 +1,26 @@
 // Set up the data for the chart
-const data = [
-    { name: "Sep 2019", value: 708.6 },
-    { name: "Mar 2020", value: 754.6 },
-    { name: "Sep 2020", value: 738.6 },
-    { name: "Mar 2021", value: 834.6 },
-    { name: "Sep 2021", value: 920.8 },
-    ];
+d3.csv("../csv/statisticsforcanada.csv").then(function(dataset)
+{
+    data = dataset;
+    waterfallChart(data);
+})
 
+function waterfallChart(data)
+{
     // Set up the dimensions of the chart
-    const width = document.querySelector('.col-sm-6').offsetWidth - 80;
-    const height = document.querySelector('#chart1').offsetHeight * 0.9;
-    var barPadding = width/data.length - (width/data.length * 0.5);
+    const margin = {top: 70, right: 30, bottom: 60, left: 80}
+    const width = document.querySelector('.col-5').offsetWidth  - margin.left - margin.right;
+    const height = document.querySelector('#chart1').offsetHeight * 0.85 - margin.top - margin.bottom;
     
     // Set up the x and y scales for the chart
     const x = d3.scaleBand()
                 .range([0, width])
-                .padding(0.1)
-                .domain(data.map((d) => d.name));
+                .domain(data.map((d) => d.CensusYears))
+                .paddingInner(0.1);
                 
     const y = d3.scaleLinear()
-                .domain([d3.min(data, (d) => d.value), d3.max(data, (d) => d.value)])
-                .range([height + barPadding - 50, barPadding]);
-
-    // Create scalable x and y-axis
-    var xAxis = d3.axisBottom()
-                .tickValues(["Sep 2019", "Sep 2020", "Sep 2021"])
-                .scale(x);
-
-    var yAxis = d3.axisLeft()
-                .ticks(data.length * 4)
-                .scale(y);
+                .domain([0, d3.max(data, (d) => +d.Number)])
+                .range([height, 0]);
 
     // Calculate the starting and ending values for each data point
     let initial = 0;
@@ -38,13 +29,13 @@ const data = [
         if (i == 0)
         {
             data[i].start = initial;
-            initial += (data[i].value);
+            initial += (+data[i].Number);
             data[i].end = initial;
         }
         else
         {
             data[i].start = initial;
-            initial += (data[i].value - data[i-1].value);
+            initial += (+data[i].Number - +data[i-1].Number);
             data[i].end = initial;
         }
     }
@@ -56,10 +47,107 @@ const data = [
     const svg = d3
     .select("#chart1")
     .append("svg")
-    .attr("width", width + 50)
-    .attr("height", height + 40)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + 50 + "," + -10 + ")");
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Add the chart title
+    svg.append("text")
+    .attr("id", "chart-title")
+    .attr("x", width / 2)
+    .attr("y", -35)
+    .attr("text-anchor", "middle")
+    .text("Population Growth in Canada");
+
+    // Add the axis labels to the chart
+    svg.append("g")
+        .attr("id", "x-axis")
+        .style("font-size", "14px")
+        .attr("font-family","Times New Roman")
+        .call(d3.axisBottom(x)
+                // .tickValues(["2017-2018", "2019-2020", "2021-2022"])
+                )
+        .call(g => g.select(".domain").remove())
+        .attr("transform", `translate(0, ${height})`);
+    
+    svg.selectAll(".tick line")
+        .attr("transform", `translate(0, ${-height})`) // move the tick lines to the top of the chart
+        .style("stroke-opacity", 0.2);
+    
+    svg.selectAll(".tick text")
+        .attr("fill", "#777")
+        .attr("y", 10)
+        .style("text-anchor", "middle");
+
+    svg.append("g")
+        .style("font-size", "14px")
+        .attr("font-family","Times New Roman")
+        .call(d3.axisLeft(y)
+                    .tickFormat(d3.format(".2s"))
+                    .ticks(data.length * 4))
+        .call(g => g.select(".domain").remove()) 
+        .selectAll(".tick text")
+        .style("fill", "#777") 
+        .style("visibility", (d, i, nodes) =>
+        {
+            if (i === 0)
+            {
+                return "hidden"; 
+            }
+            else
+            {
+                return "visible"; 
+            }
+        });
+        
+    svg.selectAll(".tick line")
+        .attr("transform", `translate(0, ${-height})`) // move the tick lines to the top of the chart
+        .style("stroke-opacity", 0.2);
+
+    // Add the x axis label
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + 40)
+        .attr("text-anchor", "middle")
+        .text("Census Years")
+        .style("font-size", "18px")
+        .attr("font-family","Times New Roman");
+
+    // Add the y axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -70)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Number of people")
+        .attr("font-family","Times New Roman")
+        .attr("font-size", "18px");
+
+    // Add vertical gridlines
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x)
+        .tickSize(-height)
+        .tickFormat(""))
+        .attr("stroke-width", .5)
+        .attr("opacity", 0.2);
+
+    // Add horizontal gridlines
+    svg.append("g")
+        .attr("class", "grid")
+        .call(d3.axisLeft(y)
+            .tickSize(-width)
+            .tickFormat("")
+        )
+        .attr("x1", 0)
+        .attr("x2", width)
+        .attr("y1", d => y(d))
+        .attr("y2", d => y(d))
+        .attr("stroke-width", .5)
+        .attr("opacity", 0.2);
 
     // Add the bars to the chart
     svg
@@ -67,8 +155,8 @@ const data = [
     .data(data)
     .enter()
     .append("rect")
-    .attr("class", (d,i) => "bar " + ((i > 0 && d.value < data[i-1].value) ? "negative" : "positive")) //use this one to customize the bar color on CSs.
-    .attr("x", (d) => x(d.name))
+    .attr("class", (d,i) => "bar " + ((i > 0 && +d.Number < +data[i-1].Number) ? "negative" : "positive")) //use this one to customize the bar color on CSs.
+    .attr("x", (d) => x(d.CensusYears))
     .attr("y", (d) => y(Math.max(d.start, d.end)))
     .attr("width", x.bandwidth())
     .attr("height", (d) => Math.abs(y(d.start) - y(d.end)))
@@ -76,27 +164,11 @@ const data = [
     .on("mouseover", function (event, d)
         {
             // Show a small box about the bar's value being hover on
-            d3.select(this).append("title").text(`${d.name}: ${d.value}`);
+            d3.select(this).append("title").text(`${d.CensusYears}: ${+d.Number}`);
 
             // Emphasize the bar being hovered on
             d3.select(this).style("opacity", 1);
             svg.selectAll(".bar:not(:hover)").style("opacity", 0.2);
-
-            var xPosition = parseFloat(d3.select(this).attr("x"));
-            var yPosition = parseFloat(d3.select(this).attr("y"));
-            var value = d;
-
-            // Adding label for each item
-            svg.append("text")
-            .attr("id","tooltip")
-            .attr("x", xPosition + x.bandwidth() / 4)
-            .attr("y",  yPosition - 10)
-            .text(value.value)
-            .attr("fill","black")
-            .attr("position", "relative")
-            .attr("z-index",1)
-            .style("font-size", "14px")
-            .attr("font-family","Times New Roman");
         })
     // When the user not hover on that bar anymore, make every other bars normal again
     .on("mouseout", function (event, d)
@@ -104,54 +176,6 @@ const data = [
             svg.selectAll(".bar").style("opacity", 1);
 
             d3.select("#tooltip").remove();
-        });
-    
-    // Add the label of each item in the chart
-    // svg.selectAll("text")
-    // .data(data)
-    // .enter()
-    // .append("text")
-    // .attr("class", "tool")
-    // .attr("x", (d) => x(d.name) + x.bandwidth() / 2 - 20)
-    // .attr("y",  (d) => y(Math.max(d.start, d.end) + 10))
-    // .text(function(d) { return d.value; })
-    // .attr("fill","black")
-    // .attr("position", "relative")
-    // .attr("z-index",1)
-    // .style("font-size", "14px")
-    // .attr("font-family","Times New Roman");
-
-    // Add the axis labels to the chart
-    svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0, " + (height - barPadding + 8.5) +")")
-        .call(xAxis)
-        .style("font-size", "14px")
-        .attr("font-family","Times New Roman");
-
-    svg.append("g")
-        .attr("class", "axis")
-        .call(yAxis)
-        .style("font-size", "14px")
-        .attr("font-family","Times New Roman");
-
-    // Add the x axis label
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width - 100)
-        .attr("y", height + 45)
-        .text("Quarter")
-        .style("font-size", "18px")
-        .attr("font-family","Times New Roman")
-        .attr("font-weight", "bold");
-
-    // Add the y axis label
-    svg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", -300)
-        .attr("y", -35)
-        .attr("transform", "rotate(-90)")
-        .text("Cost of Living ($thousand)")
-        .attr("font-family","Times New Roman")
-        .attr("font-weight", "bold")
-        .attr("font-size", "18px");
+        })
+    .attr("transform", "translate(0, 0)");
+}
